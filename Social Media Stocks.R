13789816573ec21a2b7537c89c31e69e1442df93
cameronkasper12 <61440636+cameronkasper12@users.noreply.github.com>
2020-03-29 14:58:36 -0400

    Full Sentiment Analysis tool
    Currently set to analyze 1000 tweets about Tesla using only keyword #Tesla. The tool displays both a word cloud and sentiment bar graph. 
    
    We should develop a keyword search list.
    We must begin collecting data asap on both ends (stock prices and sentiment analysis synergistic in time and day of data extraction).
    We may also research stop words, their importance (especially given the circumstances), and what words may fit in that list

--- Social Media Stocks.R
+++ Social Media Stocks.R
@@ -10,6 +10,7 @@ library("topicmodels")
 library("syuzhet")
 library("twitteR")
 library("ROAuth")
+library("ggplot2")
 
 consumer_key <- 'PIkNDOuOP1XncLI9Mtp4MEZrO'
 consumer_secret <- 'Qk4efz3C703F087zOIQtoNwD0pheSVRNg0C6jKacWRRyI0yBb9'
@@ -17,11 +18,9 @@ access_token <- '3165956396-Kt1D2Klnl9eEIbAhNmCTGLsGK5f5RvA8mRlRuTi'
 access_secret <- '1saWbKiOulpRfvxNqISBGavlX5Bh8fQDiggoLhiStuXdK'
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-tweets_t <- searchTwitter("#Tesla", n=1000,lang = "en")
+#tweets_Tesla <- searchTwitter("#Tesla", n=1000,lang = "en")
 
-Tesla_tweets <- twListToDF(tweets_t)
-
-View(Tesla_tweets)
+Tesla_tweets <- twListToDF(tweets_Tesla)
 
 Tesla_text<- Tesla_tweets$text
 
@@ -49,10 +48,26 @@ Tesla_text <- gsub("^ ", "", Tesla_text)
 # Remove blank spaces at the end
 Tesla_text <- gsub(" $", "", Tesla_text)
 
+View(Tesla_text)
+
 #clean up by removing stop words
-Tesla_tweets.text.corpus <- tm_map(Tesla_tweets.text.corpus, function(x)removeWords(x,stopwords()))
+
 
 library("wordcloud")
 #generate wordcloud
-wordcloud(Tesla_tweets.text.corpus,min.freq = 10,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 500)
+wordcloud(Tesla_text,min.freq = 10,colors=brewer.pal(8, "Dark2"),random.color = TRUE,max.words = 500)
 
+#getting emotions using in-built function
+mysentiment_Tesla <-get_nrc_sentiment((Tesla_text))
+
+#calculationg total score for each sentiment
+Sentimentscores_Tesla<-data.frame(colSums(mysentiment_Tesla[,]))
+
+names(Sentimentscores_Tesla)<-"Score"
+Sentimentscores_Tesla<-cbind("sentiment"=rownames(Sentimentscores_Tesla),Sentimentscores_Tesla)
+rownames(Sentimentscores_Tesla)<-NULL
+
+#plotting the sentiments with scores
+ggplot(data=Sentimentscores_Tesla,aes(x=sentiment,y=Score))+geom_bar(aes(fill=sentiment),stat = "identity")+
+  theme(legend.position="none")+xlab("Sentiments")+ylab("scores")+
+  ggtitle("Sentiments of people behind the tweets on Tesla")
